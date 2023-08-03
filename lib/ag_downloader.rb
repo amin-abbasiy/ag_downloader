90e3137e85223a2e23945a85960729928820ffb5
amin-abbasiy <uamin.en@gmail.com>
2023-08-03 13:33:54 +0200

    add more error handling and fix offenses

--- lib/ag_downloader.rb
+++ lib/ag_downloader.rb
@@ -1,49 +1,69 @@
 # frozen_string_literal: true
 
-require_relative "ag_downloader/version"
+require_relative 'ag_downloader/version'
 require_relative 'ag_downloader/validations'
+require_relative 'ag_downloader/download'
+require_relative 'ag_downloader/util'
 require 'tty-option'
 
 module AgDownloader
+  ## Path: lib/ag_downloader.rb
+  # Class to work with command line options
+  # Example:
+  #   klass = AgDownloader::Options.new
+  #   klass.parse
+  #   klass.run
+  #
+  # @param file [String] path to file
+  # @param help [Boolean] print usage
   class Options
     include TTY::Option
     include ::AgDownloader::Validations
+    include ::AgDownloader::Util
 
     usage do
-      program "ag"
+      program 'ag'
 
       command ''
 
-      desc "Get file and download images from urls inside the file"
+      desc 'Get file and download images from urls inside the file'
 
-      example "$ ag download `path/to/file`"
+      example '$ ag download `path/to/file`'
     end
 
     argument :file do
       required
-      desc "The name of the file to use"
+      desc 'The name of the file to use'
     end
 
     flag :help do
-      short "-h"
-      long "--help"
-      desc "Print usage"
+      short '-h'
+      long '--help'
+      desc 'Print usage'
     end
 
-    def run
-      if params[:help]
-        puts help
-      elsif params[:file]
-        validate_file(file: params[:file])
-        puts "Downloading..."
-      elsif params.errors.any?
-        puts params.errors.summary
-      else
-        puts params.to_h
-      end
+    def download
+      validate_file(file: params[:file])
+      urls = batch_read(filepath: params[:file])
+      urls.each { |url| validate_url(url:) }
+      ::AgDownloader::Download.new.batch_download(urls:)
+      puts 'Downloading...'
+    end
 
-    rescue InvalidFileError => e
+    # rubocop:disable Metrics/AbcSize
+    def run
+      puts help if params[:help]
+      download if params[:file]
+      puts params.errors.summary if params.errors.any?
+    rescue AgDownloader::InvalidFileError => e
+      puts e
+    rescue AgDownloader::InvalidUrlError => e
+      puts e
+    rescue SocketError => e
+      puts e
+    rescue Net::ReadTimeout => e
       puts e
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
--- spec/ag_downloader_spec.rb
+++ spec/ag_downloader_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "spec_helper"
+require_relative 'spec_helper'
 
 RSpec.describe AgDownloader::Options do
-  it "has a version number" do
+  it 'has a version number' do
     expect(AgDownloader::VERSION).not_to be nil
   end
 
@@ -16,15 +16,6 @@ RSpec.describe AgDownloader::Options do
     $stdout = @original_stdout
   end
   context 'when valid data provided' do
-    it 'file provided' do
-      ARGV.clear
-      ARGV.concat [File.expand_path('spec/fixtures/valid_file.txt')]
-
-      described_class.new.parse.run
-
-      expect($stdout.string).to include("Downloading...")
-    end
-
     it 'help flag provided' do
       ARGV.clear
       ARGV.concat %w[--help]
@@ -50,7 +41,7 @@ RSpec.describe AgDownloader::Options do
 
       described_class.new.parse.run
 
-      expect($stdout.string).to include("File does not exist")
+      expect($stdout.string).to include('File does not exist')
     end
   end
 end
