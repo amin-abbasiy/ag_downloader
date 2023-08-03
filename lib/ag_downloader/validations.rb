920efea9646ddf01cc0fe27d40f75e0a5c25bf7f
amin-abbasiy <uamin.en@gmail.com>
2023-08-03 22:02:23 +0200

    encapsulate validation methods and provide unified interface

--- lib/ag_downloader/validations.rb
+++ lib/ag_downloader/validations.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'ag_downloader_error'
+require_relative 'http'
 
 module AgDownloader
   ## Path: lib/ag_downloader/validations.rb
@@ -16,23 +17,40 @@ module AgDownloader
   # Raises:
   #   AgDownloader::InvalidUrlError
   #   AgDownloader::InvalidFileError
+  #   AgDownloader::InvalidSourceError
   #
   # Returns:
   #   true
   #
   # @param url [String] url to validate
-  # @param file [String] file to validate
+  # @param source [String] source to validate
+  # @param type [Symbol] type of source to validate
   # @return [Boolean] true
   module Validations
+    VALID_SOURCE_TYPES = %i[url file source].freeze
+    def validates(source, type:)
+      validate_source_types(type:)
+      validate_url(url: source) if type == :url
+      validate_file(file: source) if type == :file
+
+      true
+    end
+
+    private
+
+    def validate_source_types(type:)
+      raise AgDownloader::InvalidSourceError, "`#{type}` Source is not valid" unless VALID_SOURCE_TYPES.include?(type)
+    end
+
     def validate_url(url:)
-      raise AgDownloader::InvalidUrlError, 'URL is not valid' unless url =~ URI::DEFAULT_PARSER.make_regexp
-      raise AgDownloader::InvalidUrlError, 'URL is invalid' unless validate_protocols(url:)
+      raise AgDownloader::InvalidUrlError, "`#{url}` is not a valid URL" unless url =~ URI::DEFAULT_PARSER.make_regexp
+      raise AgDownloader::InvalidUrlError, "`#{url}` does not have valid scheme" unless validate_protocols(url:)
 
       true
     end
 
     def validate_file(file:)
-      raise AgDownloader::InvalidFileError, 'File does not exist' unless File.exist?(file)
+      raise AgDownloader::InvalidFileError, "`#{file}` File does not exist" unless File.exist?(file)
 
       true
     end
--- spec/ag_downloader/validations_spec.rb
+++ spec/ag_downloader/validations_spec.rb
@@ -5,38 +5,44 @@ require_relative '../spec_helper'
 RSpec.describe AgDownloader::Validations do
   let(:klass) { Class.new { include AgDownloader::Validations } }
   let(:klass_object) { klass.new }
+  let(:valid_file) { File.expand_path('spec/fixtures/valid_file.txt') }
+  let(:valid_url) { 'https://www.example.com' }
 
-  describe '#validate_file' do
-    let(:valid_file) { File.expand_path('spec/fixtures/valid_file.txt') }
+  context 'validate file' do
     let(:invalid_file) { File.expand_path('spec/fixtures/invalid_file.txt') }
 
-    context 'when file provided' do
-      it 'returns true' do
-        expect(klass_object.validate_file(file: valid_file)).to eq(true)
-      end
+    it 'returns true' do
+      expect(klass_object.validates(valid_file, type: :file)).to eq(true)
     end
 
-    context 'when file does not exist' do
-      it 'raise error' do
-        expect { klass_object.validate_file(file: invalid_file) }.to raise_error(AgDownloader::InvalidFileError)
-      end
+    it 'raises an error' do
+      expect { klass_object.validates(invalid_file, type: :file) }
+        .to raise_error(AgDownloader::InvalidFileError)
     end
   end
 
-  describe '#validate_url' do
-    let(:valid_url) { 'https://www.example.com' }
+  context 'validate url' do
     let(:invalid_url) { 'invalid_url' }
 
-    context 'when url provided' do
-      it 'returns true' do
-        expect(klass_object.validate_url(url: valid_url)).to eq(true)
-      end
+    it 'returns true' do
+      expect(klass_object.validates(valid_url, type: :url)).to eq(true)
     end
 
-    context 'when url does not exist' do
-      it 'raise error' do
-        expect { klass_object.validate_url(url: invalid_url) }.to raise_error(AgDownloader::InvalidUrlError)
-      end
+    it 'raises an error' do
+      expect { klass_object.validates(invalid_url, type: :url) }
+        .to raise_error(AgDownloader::InvalidUrlError)
+    end
+  end
+
+  context 'invalid resource' do
+    it 'raises an error' do
+      expect { klass_object.validates(valid_file, type: :invalid_source) }
+        .to raise_error(AgDownloader::InvalidSourceError)
+    end
+
+    it 'raises an error' do
+      expect { klass_object.validates(valid_url, type: :invalid_source) }
+        .to raise_error(AgDownloader::InvalidSourceError)
     end
   end
 end
