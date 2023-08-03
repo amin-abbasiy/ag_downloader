01ff92680efca963a26356c0d3acb119fba9d9aa
amin-abbasiy <uamin.en@gmail.com>
2023-08-03 13:08:43 +0200

    update test config and assets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,13 @@
 
 require_relative '../lib/ag_downloader'
 require_relative '../lib/ag_downloader/validations'
+require_relative '../lib/ag_downloader/download'
+require_relative '../lib/ag_downloader/util'
+require_relative '../lib/ag_downloader/http'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
