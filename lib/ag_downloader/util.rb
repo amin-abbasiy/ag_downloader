53442bc316b910c976ceb07da25b143110b86ade
amin-abbasiy <uamin.en@gmail.com>
2023-08-03 21:55:10 +0200

    add url support for source file to util

--- lib/ag_downloader/util.rb
+++ lib/ag_downloader/util.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-require 'net/http'
+require_relative 'http'
+require_relative 'validations'
 
 module AgDownloader
   ## Path: lib/ag_downloader/util.rb
   # Module to work with files
-  # Module includes method to read file and split it by spaces
+  # Module includes method to read from file or url and split it by spaces
   #
   # Example:
   #  klass = Class.new { include AgDownloader::Util }
@@ -13,10 +14,29 @@ module AgDownloader
   #  klass_object.batch_read(filepath: 'path/to/file')
   #
   # @param filepath [String] path to file
+  # @param type [Symbol] type of source
   # @return [Array] array of urls
   module Util
-    def batch_read(filepath:)
-      File.open(filepath, 'r').read.split(' ')
+    include ::AgDownloader::Validations
+    def batch_read(source:, type:)
+      validates(source, type: :source)
+      return read_from_disk(file: source) if type&.to_sym == :file
+      return read_from_url(url: source) if type&.to_sym == :url
+
+      raise AgDownloader::InvalidSourceError, 'Source type is not valid'
+    end
+
+    def read_from_url(url:)
+      validates(url, type: :url)
+      http = AgDownloader::Http.new(url:)
+      http.send_request(method: :get) do |response|
+        return response.body.split(' ')
+      end
+    end
+
+    def read_from_disk(file:)
+      validates(file, type: :file)
+      File.open(file, 'r').read.split(' ')
     end
   end
 end
--- spec/ag_downloader/util_spec.rb
+++ spec/ag_downloader/util_spec.rb
@@ -1,17 +1,46 @@
 # frozen_string_literal: true
 
 require_relative '../spec_helper'
+require 'webmock/rspec'
 
 RSpec.describe AgDownloader::Util do
   let(:klass) { Class.new { include AgDownloader::Util } }
   let(:klass_object) { klass.new }
-  let(:valid_path) { File.expand_path('spec/fixtures/valid_file.txt') }
+  let(:valid_file) { File.expand_path('spec/fixtures/valid_file.txt') }
   let(:image_path) { File.expand_path('spec/fixtures/image.jpeg') }
+  let(:valid_url) { 'https://example.com' }
+  let(:invalid_file) { 'invalid_file' }
+  let(:invalid_url) { 'invalid_url' }
 
-  context 'when file provided' do
+  context 'when source provided' do
     it 'returns true' do
-      expect(klass_object.batch_read(filepath: valid_path)).to eq(%w[https://www.example.com/file1.png
-                                                                     https://www.example.com/file2.png])
+      expect(klass_object.batch_read(source: valid_file, type: :file)).to eq(%w[https://www.example.com/file1.png
+                                                                                https://www.example.com/file2.png])
+    end
+
+    it 'reads from the url and splits by spaces' do
+      stub_request(:get, valid_url)
+        .to_return(status: 200, body: 'https://www.example.com/file1.png https://www.example.com/file2.png')
+
+      result = klass_object.batch_read(source: valid_url, type: :url)
+      expect(result).to eq(%w[https://www.example.com/file1.png https://www.example.com/file2.png])
+    end
+  end
+
+  context 'when source is not valid' do
+    it 'raises an error for invalid file' do
+      expect { klass_object.batch_read(source: invalid_file, type: :file) }
+        .to raise_error(AgDownloader::InvalidFileError)
+    end
+
+    it 'raises an error for invalid source' do
+      expect { klass_object.batch_read(source: valid_file, type: :invalid) }
+        .to raise_error(AgDownloader::InvalidSourceError)
+    end
+
+    it 'raises an error for invalid url' do
+      expect { klass_object.batch_read(source: invalid_url, type: :url) }
+        .to raise_error(AgDownloader::InvalidUrlError)
     end
   end
 end
