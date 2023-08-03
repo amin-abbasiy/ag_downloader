bb3be9f3ec414b5451b8a0b97f58d6cec5b031ef
amin-abbasiy <uamin.en@gmail.com>
2023-08-03 22:05:15 +0200

    update error class

--- lib/ag_downloader/ag_downloader_error.rb
+++ lib/ag_downloader/ag_downloader_error.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 module AgDownloader
   class AgDownloaderError < StandardError; end
 
   class InvalidUrlError < AgDownloaderError; end
 
   class InvalidFileError < AgDownloaderError; end
+
+  class InvalidSourceError < AgDownloaderError; end
 end
